27748e0ffbbafa0f7aa1c0d34b68827a1d94ced8
Larry Leon <larry.leon.05@post.harvard.edu>
2025-10-20 03:51:56 -0700

    remove progressr

--- dev/devtools-helpers_forestsearch.R
+++ dev/devtools-helpers_forestsearch.R
@@ -29,8 +29,6 @@ desc::desc_set_dep(package = "weightedSurv", type = "Imports")
 
 desc::desc_set_remotes("larry-leon/weightedSurv")
 
-usethis::use_package("progressr", type = "Imports")
-
 usethis::use_package("patchwork", type = "Suggests")
 
 
