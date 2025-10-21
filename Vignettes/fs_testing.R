f546130818ab782242d6dddf137f6c8eff03dd8d
Larry Leon <larry.leon.05@post.harvard.edu>
2025-10-21 15:05:36 -0700

    update docs

--- Vignettes/fs_testing.R
+++ Vignettes/fs_testing.R
@@ -27,18 +27,44 @@ registerDoFuture()
 registerDoRNG()
 
 
+# Example with pre-specified subgroup via conf_force argument
+# Note that er and pgr could be important "biomarkers" and we pre-specify "low expression cuts"
+# up to the 1st quartiles which are 8 (er) and 7 (pgr);
+# Note that by include "low expression" values the complementary levels are automatically considered.
+# For example "er <= 0" means "er > 0" evaluated (etc)
+
 system.time({fs <- forestsearch(df_gbsg,  confounders.name=confounders.name,
                    outcome.name = "tte", treat.name = "treat", event.name = "event", id.name = "id",
                    hr.threshold = 1.25, hr.consistency = 1.0, pconsistency.threshold = 0.90,
                    sg_focus = "hrMaxSG",
                    showten_subgroups = FALSE, details=TRUE,
-                   conf_force = c("age <= 65", "er <= 0", "er <= 1", "er <= 2"," er <= 5"),
+                   conf_force = c("age <= 65", "er <= 0", "er <= 1", "er <= 2"," er <= 5", "er <= 8",
+                                  "pgr <= 0", "pgr <= 1", "pgr <= 2", "pgr <= 5", "pgr <= 7"),
+                   max_subgroups_search = 12,
                    cut_type = "default", use_grf = TRUE, plot.grf = TRUE, use_lasso = TRUE,
-                   maxk = 2, n.min = 60, d0.min = 10, d1.min = 10,
+                   maxk = 2, n.min = 60, d0.min = 12, d1.min = 12,
                    plot.sg = TRUE, by.risk = 12,
                    parallel_args = list(plan="callr", workers = 12, show_message = TRUE)
                    )
 })
+
+
+
+
+# Re-produce analysis in paper
+system.time({fs_paper <- forestsearch(df_gbsg,  confounders.name=confounders.name,
+                                outcome.name = "tte", treat.name = "treat", event.name = "event", id.name = "id",
+                                hr.threshold = 1.25, hr.consistency = 1.0, pconsistency.threshold = 0.90,
+                                sg_focus = "hrMaxSG",
+                                showten_subgroups = FALSE, details=TRUE,
+                                conf_force = NULL,
+                                cut_type = "default", use_grf = TRUE, plot.grf = TRUE, use_lasso = TRUE,
+                                maxk = 2, n.min = 60, d0.min = 10, d1.min = 10,
+                                plot.sg = TRUE, by.risk = 12,
+                                parallel_args = list(plan="callr", workers = 12, show_message = TRUE)
+)
+})
+
 
 # reset workeres
 plan("sequential")
@@ -52,7 +78,7 @@ options(warn = -1)
 output_dir <- "results/"
 save_results <- dir.exists(output_dir)
 
-NB <- 100
+NB <- 20
 
 t.start <- proc.time()[3]
 
@@ -65,34 +91,22 @@ t.min<-(t.now-t.start)/60
 cat("Minutes (total) for bootstrap (boots,mins)",c(NB,t.min),"\n")
 cat("Projected minutes for 1000",c(t.min*(1000/NB)),"\n")
 
+if (save_results) {
+  filename <- file.path(output_dir,
+                        paste0("gbsg_results_B=",
+                               format(NB),
+                               ".RData"))
+  save(fs_bc, fs, file = filename)
+  cat("\nResults saved to:", filename, "\n")
+}
 
-# Access the new tables
-#diagnostics_df <- fs_bc$summary$diagnostics_table
 
-
+# SG estimates
 fs_bc$summary$table
-
 # View the diagnostics table
 fs_bc$summary$diagnostics_table_gt
-
-
 #View the formatted timing table
 fs_bc$summary$timing$time_table_gt
 
-
-
-
-# Or access it directly
-print(fs_bc$summary$timing$time_table_gt)
-
-
-# View as data frames
-#print(diagnostics_df)
-#print(timing_df)
-
-# Or convert to gt tables for nice formatting
-library(gt)
-#gt::gt(diagnostics_df)
-gt::gt(timing_df)
 
 
